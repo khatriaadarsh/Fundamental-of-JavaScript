79409c78304ae08f614e5cba3488185f97d16597
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-14 00:07:07 +0500

    Apply comments

--- Script.js
+++ Script.js
@@ -17,24 +17,24 @@
 
       // const numbers = [1,2,3,4,5];
          // Values of numbers passing by num and then return the double of the num.
-      // const doubleNumbers = numbers.map((num)=>{
+      // const doubleNumbers = numbers.map( (num) => {
       //      return num * 2;
       // });
       // console.log(doubleNumbers);
 
    //==> Filter: when we need some elemnt from array then use filter 
    //   let numbers = [1,2,3,4,5];
-   //   let evenNumbers = numbers.filter((val) =>{
+   //   let evenNumbers = numbers.filter( (val) => {
    //    return val % 2 === 0;
    //   })
    //   console.log(evenNumbers);
 
 //   ==> Find:  the find() method is used to search for a specific element in an array based on a provided condition. 
 // find() is useful when you want to find the first occurrence of an element in an array that matches a specific condition.
-       let numbers = [1,2,3,4,5,6];
-       let found = numbers.find((val)=>{
-         return val > 2;
-       }) 
-       console.log(found)
+      //  let numbers = [1,2,3,4,5,6];
+      //  let found = numbers.find( (val) => {
+      //    return val > 2;
+      //  }) 
+      //  console.log(found);
        
 
