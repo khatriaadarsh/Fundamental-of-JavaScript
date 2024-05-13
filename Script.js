5c884c43ad8d24dd04f8390346f1ae7ce5d8d2a5
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-13 23:47:08 +0500

    Define Filter with Example

--- Script.js
+++ Script.js
@@ -10,14 +10,22 @@
     //     console.log(val + ":Student")
     // });
 
-   //--->Map : Map is higher order function that is used to iterate over the array and transform each element of    the   each array according to callback function 
+   //==>Map : Map is higher order function that is used to iterate over the array and transform each element of    the   each array according to callback function 
       // -> The map() function return a new array containing the result of applying callback function to each element of the orginal array 
 
       // map() is a versatile and commonly used array method for transforming array elements or generating new arrays based on existing ones.
-      
-      const numbers = [1,2,3,4,5];
-        //Values of numbers passing by num and then return the double of the num
-      const doubleNumbers = numbers.map((num)=>{
-           return num * 2;
-      });
-      console.log(doubleNumbers);
+
+      // const numbers = [1,2,3,4,5];
+         // Values of numbers passing by num and then return the double of the num.
+      // const doubleNumbers = numbers.map((num)=>{
+      //      return num * 2;
+      // });
+      // console.log(doubleNumbers);
+
+   //==> Filter : when we need some elemnt from array then we use filter 
+   //   let numbers = [1,2,3,4,5];
+   //   let evenNumbers = numbers.filter((val) =>{
+   //    return val % 2 === 0;
+   //   })
+   //   console.log(evenNumbers);
+
